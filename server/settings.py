6a5b7612388c3050cb8b75498bd5cccf39c6fedc
kevinhohe <1146973131@qq.com>
2025-10-15 17:42:12 -0600

    finished making entries public and added entry model.

--- server/settings.py
+++ server/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'server.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / "templates"],  # for project-level templates
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [

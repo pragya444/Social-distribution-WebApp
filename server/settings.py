8a08a1a907bca5b8c7787b733e07029c1d76199d
kevinhohe <1146973131@qq.com>
2025-11-15 17:42:29 -0700

    ready for heroku

--- server/settings.py
+++ server/settings.py
@@ -24,7 +24,7 @@ LOCAL_API_BASE = "http://127.0.0.1:8000/api/"
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ["SECRET_KEY"]
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True

f98d6b5e32a76a70ff8a6d23a0e54a4812828551
Amitoj Singh <136754478+amitojxsingh@users.noreply.github.com>
2025-11-19 12:52:06 +0530

    Enable DEBUG mode in settings.py
    Set DEBUG mode to True for development.

--- server/settings.py
+++ server/settings.py
@@ -29,7 +29,7 @@ dotenv.load_dotenv(BASE_DIR / ".env")
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv("DEBUG", "0") == "1"
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 

a0e743e3b53af6df9a05470f723d38d53063462d
Pragya Das <pragya4@ualberta.ca>
2025-11-29 10:01:34 +0000

    Revert "add pagination UI"
    This reverts commit afc5167dc0b758ba9703ea135390d5cf9abd33d8.

--- api/entries/entryView.py
+++ api/entries/entryView.py
@@ -459,7 +459,6 @@ class EntryView(APIView):
                 'author': author,
                 'entries': page_obj.object_list,
                 'tab': 'all',
-                'page_obj': page_obj,          # added
             }, template_name='author_all_entries.html')
         
         # JSON response

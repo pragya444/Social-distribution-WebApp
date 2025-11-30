b5db05c42270129e35128b5e6322b29bb7b3b943
Pragya Das <pragya4@ualberta.ca>
2025-11-30 10:08:10 +0000

    fixing host

--- api/follow/followViews.py
+++ api/follow/followViews.py
@@ -1075,9 +1075,10 @@ def fetch_remote_authors_from_node(node):
                 user = inbox_view.get_or_create_remote_user(normalized)
                 if user and user.url:
                     remote_users.append(user)
+                return remote_users
             except Exception as e:
                 log.exception(f"Failed processing author: {e}")
-                continue
+                return []
 
     except Exception as e:
         log.exception(f"Fatal error fetching authors from {node.host}: {e}")
